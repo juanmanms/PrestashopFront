c34974f1c8f07e7833fe6d297be5b1e50cc48ff2
Juanmanms <juanmanms@gmail.com>
2024-08-14 09:58:14 +0200

    Enlace a home

--- src/components/shared/Header.jsx
+++ src/components/shared/Header.jsx
@@ -16,7 +16,7 @@ export const Header = () => {
         <>
             <nav className="bg-white border-gray-200 dark:bg-gray-900">
                 <div className="flex flex-wrap justify-between items-center mx-auto max-w-screen-xl p-4">
-                    <a href="https://flowbite.com" className="flex items-center space-x-3 rtl:space-x-reverse">
+                    <a href={process.env.REACT_APP_URL_HOME} className="flex items-center space-x-3 rtl:space-x-reverse">
 
                         <span className="self-center text-2xl font-semibold whitespace-nowrap dark:text-white">Torreblanca</span>
                     </a>
