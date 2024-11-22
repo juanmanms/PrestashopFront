2406332c3e209c82cd0b88ca28b75e93539f3aa9
Juanmanms <juanmanms@gmail.com>
2024-11-22 16:49:23 +0100

    control vendedor dashboard

--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -6,21 +6,21 @@ const HomePage = () => {
     const user = useSelector((state) => state.user);
     //funcion para eliminar token de localstorage
 
+
     return (
         <div className="bg-gray-200 p-4">
-
             <h1 className="text-2xl font-bold mb-4">Dashboard del Vendedor:<br />
                 <span className='bg-red-200'>{user.name}</span>
             </h1>
             <p className="mb-4">Bienvenido a tu panel de control, aquí puedes gestionar tus productos y ventas</p>
 
-            {/* Aquí puedes añadir más componentes o lógica, como listados de productos, gráficas de ventas, etc. */}
-            <Link to="/order" className="bg-blue-500 text-white px-4 py-2 rounded hover:bg-blue-600">
-                Crear Pedido
-            </Link>
-
+            {user.id_seller && (
+                <Link to="/order" className="bg-blue-500 text-white px-4 py-2 rounded hover:bg-blue-600">
+                    Crear Pedido
+                </Link>
+            )}
         </div>
-    )
+    );
 }
 
 export default HomePage
