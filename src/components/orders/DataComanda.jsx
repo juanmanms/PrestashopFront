04590972372ae6c43a737d85104c444d98e0f8b7
Juanmanms <juanmanms@gmail.com>
2025-04-16 15:36:00 +0200

    Refactor DataComanda component to improve date handling and optimize delivery days fetching

--- src/components/orders/DataComanda.jsx
+++ src/components/orders/DataComanda.jsx
@@ -1,8 +1,7 @@
 import { DatePicker, message } from 'antd';
 import DeliveryService from '../../common/service/deliveryService';
 import PropTypes from 'prop-types';
-import { useEffect, useState, useMemo } from 'react';
-
+import { useEffect, useState, useMemo, useCallback } from 'react';
 import dayjs from 'dayjs';
 import customParseFormat from 'dayjs/plugin/customParseFormat';
 import es from 'antd/es/date-picker/locale/es_ES';
@@ -13,94 +12,82 @@ import isBetween from 'dayjs/plugin/isBetween';
 dayjs.extend(utc);
 dayjs.extend(timezone);
 dayjs.extend(isBetween);
-
 dayjs.extend(customParseFormat);
+
+const initialTime = dayjs().tz('Europe/Madrid').set({ 'hour': 2, 'minute': 0, 'second': 0 });
 
 export const DataComanda = ({ setStartDate, startDate, carrier }) => {
     const deliveryService = useMemo(() => DeliveryService(), []);
     const [dias, setDias] = useState([]);
     const [loading, setLoading] = useState(true);
-
-    useEffect(() => {
-        setStartDate(dayjs().tz('Europe/Madrid').set('hour', 2).set('minute', 0).set('second', 0));
-        const fetchDias = async () => {
-            try {
-                const response = await deliveryService.getDeliveryDays();
-                const data = await response.json();
-                setDias(data);
-            } catch (error) {
-                console.error('Error fetching delivery days:', error);
-                message.error('Error fetching delivery days');
-            } finally {
-                setLoading(false);
-            }
-        };
-        fetchDias();
-    }, [deliveryService]);
-
-    const disabledDays = useMemo(() => {
-        if (carrier === parseInt(process.env.REACT_APP_recogida)) {
-            return [0]; // Solo deshabilitar domingos
-        } else {
-            return dias
-                .filter(day => day.is_active === 0)
-                .map(day => dayjs().day(day.id_delivery_day).day());
-        }
-    }, [carrier, dias]);
-
-    const disabledDate = current => {
-        if (!current) return false;
-        return disabledDays.includes(current.day());
-    };
-
     const now = dayjs().tz('Europe/Madrid');
     const currentHour = now.hour();
     const currentDay = now.day();
 
-    const minDate = (() => {
-        const today = dias.find(day => day.id_delivery_day === currentDay);
-        if (today && currentHour < dayjs(today.end_time, 'HH:mm').hour()) {
-            return now;
+    const fetchDeliveryDays = useCallback(async () => {
+        try {
+            const response = await deliveryService.getDeliveryDays();
+            const data = await response.json();
+            setDias(data);
+        } catch (error) {
+            console.error('Error fetching delivery days:', error);
+            message.error('Error fetching delivery days');
+        } finally {
+            setLoading(false);
         }
-        return now.add(1, 'day');
-    })();
+    }, [deliveryService]);
 
-    const handleDate = (date) => {
-        if (date && !date.isSame(dayjs(), 'day')) {
-            if (confirm('Has seleccionado una fecha diferente a la de hoy. ¿Quieres continuar?')) {
-                setStartDate(date ? date.tz('Europe/Madrid').add(2, 'hour') : null);
-            } else {
-                // Si el usuario cancela, puedes restablecer la fecha al valor actual o hacer otra cosa
-                // Por ejemplo, restablecer la fecha al día actual:
-                setStartDate(dayjs().tz('Europe/Madrid').set('hour', 2).set('minute', 0).set('second', 0));
-            }
-        } else {
+    useEffect(() => {
+        fetchDeliveryDays();
+        setStartDate(initialTime.hour(2).minute(0).second(0));
+
+    }, [fetchDeliveryDays, setStartDate]);
+
+    const disabledDays = useMemo(() => {
+        const isRecogida = carrier === parseInt(process.env.REACT_APP_recogida);
+        return isRecogida ? [0] : dias.filter(day => !day.is_active).map(day => dayjs().day(day.id_delivery_day).day());
+    }, [carrier, dias]);
+
+    const disabledDate = useCallback(current => {
+        return current && disabledDays.includes(current.day());
+    }, [disabledDays]);
+
+    const minDate = useMemo(() => {
+        const today = dias.find(day => day.id_delivery_day === currentDay);
+        const endTimeHour = today ? dayjs(today.end_time, 'HH:mm').hour() : 0;
+        return (today && currentHour < endTimeHour) ? now : now.add(1, 'day');
+    }, [currentDay, currentHour, dias, now]);
+
+    const handleDate = useCallback((date) => {
+        const isDifferentDay = date && !date.isSame(now, 'day');
+
+        const confirmChange = () => {
             setStartDate(date ? date.tz('Europe/Madrid').add(2, 'hour') : null);
+            document.activeElement?.blur();
+        };
+
+        if (isDifferentDay) {
+            confirm('Has seleccionado una fecha diferente a la de hoy. ¿Quieres continuar?') ? confirmChange() : setStartDate(initialTime.hour(2).minute(0).second(0));
+        } else {
+            confirmChange();
         }
         document.activeElement.blur();
-    }
+    }, [setStartDate, now]);
 
-    const spanishLocale = {
+    const spanishLocale = useMemo(() => ({
         ...es,
-        DatePicker: {
-            lang: es,
-        }
-    };
+        DatePicker: { lang: es }
+    }), []);
 
     return (
         <div>
             <h2>Fecha de entrega</h2>
-            {loading ? (
-                <p>Cargando...</p>
-            ) : (
+            {loading ? (<p>Cargando...</p>) : (
                 <DatePicker
                     aria-label='Fecha de Comanda'
                     label="Fecha de Comanda"
-                    onChange={(date) => handleDate(date)}
-                    format={{
-                        format: 'DD-MM-YYYY',
-                        type: 'mask',
-                    }}
+                    onChange={handleDate}
+                    format="DD-MM-YYYY"
                     minDate={minDate}
                     disabledDate={disabledDate}
                     locale={spanishLocale}
@@ -109,12 +96,12 @@ export const DataComanda = ({ setStartDate, startDate, carrier }) => {
                 />
             )}
         </div>
-    )
-}
+    );
+};
 
 DataComanda.propTypes = {
     setStartDate: PropTypes.func.isRequired,
     startDate: PropTypes.object,
     carrier: PropTypes.number,
-}
+};
 
