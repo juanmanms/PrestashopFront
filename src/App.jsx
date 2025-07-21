a8bc544eef2ff35aee20b04ee4d9a1c6c3c064e5
Juanmanms <juanmanms@gmail.com>
2025-07-21 09:05:55 +0200

    Remove routing and HorariosIframe component from App

--- src/App.jsx
+++ src/App.jsx
@@ -3,8 +3,7 @@ import { useEffect, useState } from 'react'
 import Login from './components/login/login'
 import SellerDashboard from './layaut/SellerDashboard';
 import { useSelector } from 'react-redux';
-import HorariosIframe from './pages/HorariosIframe';
-import { Route, BrowserRouter as Router, Routes } from 'react-router-dom';
+
 
 function App() {
   const user = useSelector((state) => state.user);
@@ -18,14 +17,8 @@ function App() {
   }, [user, auth]);
 
 
-  return (
-    <Router>
-      <Routes>
-        <Route path="/" element={auth ? <SellerDashboard /> : <Login />} />
-        <Route path="/horarios-iframe" element={<HorariosIframe />} />
-      </Routes>
-    </Router>
-  );
+  return auth ? (<SellerDashboard />) : (<Login />);
+  //return (<Login />);
 }
 
 export default App
